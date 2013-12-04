31817e896aa5e8834da547bed6b2d24838b01e2e
sa- <samay.kapadia@gmail.com>
2013-12-04 12:39:00 -0500

    Ozzing customize

--- html/customize.php
+++ html/customize.php
@@ -48,11 +48,83 @@
 ?> 
 
 <html>
+<head>
+	<link rel="stylesheet" href="p2styles.css" type = "text/css">
+	<title>Customize your schedule</title>
+	
+	<script>
+		
+		var moreCourses	= new Array();
+		var haveCredit	= new Array();
+		
+		function addCourse(name){
+			moreCourses.push(name);
+			var html= "<ul>";
+			for(i=0;i<moreCourses.length;i++){
+				html += "<li>" + moreCourses[i] +" </li>";
+			}
+			html+="</ul>";
+			document.getElementById('addcourses').value = '';
+			document.getElementById('addlist').innerHTML = html;
+		}
+		
+		function addCredit(name){
+			haveCredit.push(name);
+			var html= "<ul>";
+			for(i=0;i<haveCredit.length;i++){
+				html += "<li>" + haveCredit[i] +" </li>";
+			}
+			html+="</ul>";
+			document.getElementById('havecredit').value = '';
+			document.getElementById('havecreditlist').innerHTML = html;
+		}
+	</script>
+</head>
 <body>
-	<?php 
-		enumerateTracks(1); 
-		echo "<br>something";
-	?>
+	<div id="left">
+		<h2>Major</h2>
+			<p><ul>
+			<li>Computer Science B.S.</li>
+			</ul></p>
+		<h2>Minor</h2>
+			<p><ul>
+			<li>Mathematics</li>
+			</ul></p>
+		<h2>Cluster</h2>
+			<p><ul>
+			<li>Ethics and Values</li>
+			<li>Psychology as a Social Science</li>
+			</ul></p>
+	</div>
+	
+	<div id="center">
+		<h1>Customize your schedule</h1>
+		<h2>Major</h2>
+		
+		<div id="centermajor">
+			
+		</div>
+		
+		<div id="centerminor">
+		
+		</div>
+		
+		<div id="centercluster">
+		
+		</div>
+	</div>
+	
+	<div id="right">
+		<label for="addcourses">Other Courses to Add</label>
+		<input id="addcourses">
+		<button onClick="addCourse(document.getElementById('addcourses').value)">Add</button>
+		<div id="addlist"></div>
+		
+		<label for="havecredit">Courses you have credit for</label>
+		<input id="havecredit">
+		<button onClick="addCredit(document.getElementById('havecredit').value)">Add</button>
+		<div id="havecreditlist"></div>
+	</div>
 </body>
 </html>
 <?php mysqli_close($con); ?>
